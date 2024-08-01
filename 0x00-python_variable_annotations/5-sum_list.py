7dc1c16a1e737bab24ccc498fcbb1d6696e78628
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-08-01 14:23:02 -0400

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+function sum_list which takes
+a list input_list of floats as argument and returns their sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Function to calculate the sum of a list of floats.
+
+    Args:
+    input_list (list): List of floats.
+
+    Returns:
+    float: Sum of the input list.
+    """
+    return sum(input_list)
