68ac5933b29e6a7f5e8c581b9b2203cef6fa92c2
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-08-26 12:33:25 -0400

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,11 +9,21 @@ from utils import access_nested_map, get_json, memoize
 class TestAccessNestedMap(unittest.TestCase):
     """implementation of Test class for utils.access_nested_map"""
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ("case1", {"a": 1}, ("a",), 1),
+        ("case2", {"a": {"b": 2}}, ("a",), {"b": 2}),
+        ("case3", {"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
         """method tests Access nested map with key path parameters"""
         tested_output = access_nested_map(nested_map, path)
         self.assertEqual(tested_output, expected_result)
+
+    @parameterized.expand([
+        ("case1", {}, ("a",)),
+        ("case2", {"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, error_result):
+        """tests if error raised when keys not found"""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+            self.assertEqual(error_result, e.exception)
