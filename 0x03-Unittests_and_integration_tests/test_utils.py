8b04e1c7ba16c1d3e92ec474084e617e414a0327
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-08-26 12:38:11 -0400

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,11 +19,11 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(tested_output, expected_result)
 
     @parameterized.expand([
-        ({}, ("a",) 'a'),
+        ({}, ("a",), 'a'),
         ({"a": 1}, ("a", "b"), 'b')
     ])
     def test_access_nested_map_exception(self, nested_map, path, error_result):
         """tests if error raised when keys not found"""
-        with self.assertRaises(KeyError) as e:
+        with self.assertRaises(KeyError) as err:
             access_nested_map(nested_map, path)
-            self.assertEqual(error_result, e.exception)
+            self.assertEqual(error_result, err.exception)
