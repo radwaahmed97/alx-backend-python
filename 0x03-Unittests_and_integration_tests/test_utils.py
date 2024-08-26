2fb6e153e4b46ec1a391564a23e5d4de2952db37
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-08-26 12:35:32 -0400

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,9 +9,9 @@ from utils import access_nested_map, get_json, memoize
 class TestAccessNestedMap(unittest.TestCase):
     """implementation of Test class for utils.access_nested_map"""
     @parameterized.expand([
-        ("case1", {"a": 1}, ("a",), 1),
-        ("case2", {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ("case3", {"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
         """method tests Access nested map with key path parameters"""
@@ -19,8 +19,8 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(tested_output, expected_result)
 
     @parameterized.expand([
-        ("case1", {}, ("a",) 'a'),
-        ("case2", {"a": 1}, ("a", "b"), 'b')
+        ({}, ("a",) 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
     ])
     def test_access_nested_map_exception(self, nested_map, path, error_result):
         """tests if error raised when keys not found"""
